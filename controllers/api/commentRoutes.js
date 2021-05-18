8d340c55c871f608ab7cc00443b5adb78598d7a8
kjwallac <kristinajwallace@gmail.com>
2021-05-17 17:19:08 -0700

    added comments, added theme and styling to all pages

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,12 +4,16 @@ const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
+    console.log({reqBody: req.body});
+    const {post_Id, body}=req.body;
     const newComment = await Comment.create({
-      ...req.body,
+      post_Id: parseInt(post_Id), 
+      body,
       user_Id: req.session.user_Id,
     });
     res.json(newComment);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
